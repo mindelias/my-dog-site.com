aef368c973abc3d914b4bcf1ed809fb3180dcbb1
mindelias@gmail.com <mindelias@gmail.com>
2020-05-28 20:47:38 +0100

    add navbar component

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import NavWrapper from "./style";
+
+function Navbar() {
+  return (
+    <NavWrapper>
+      <div className="nav-container">
+        <h1>
+          <a className="logo" href="/">
+            <span className="fas fa-laugh-wink"></span>GIPHY
+             
+          </a>
+        </h1>
+      </div>
+    </NavWrapper>
+  );
+}
+
+export default Navbar;
