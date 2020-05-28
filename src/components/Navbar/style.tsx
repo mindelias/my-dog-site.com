6c8a3f69f4512753ede000e672a4c904200d5f2e
mindelias@gmail.com <mindelias@gmail.com>
2020-05-28 20:48:45 +0100

    Add search component

---
+++ src/components/Navbar/style.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const NavWrapper = styled.div`
+  padding: 1.1rem;
+  background: white;
+  h1 a {
+    font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
+      "Lucida Sans Unicode", Geneva, Verdana, sans-serif;
+    font-size: 3.5rem;
+    font-weight: 900;
+    color: #3b0831;
+  }
+`;
+export default NavWrapper
---
+++ src/components/Search/index.tsx
@@ -0,0 +1,67 @@
+import React, { useContext, useState, useEffect } from "react";
+import { connect } from "react-redux";
+import { Istate } from "../redux/Giphy/giphy.reducer";
+import { FilterGifs, ClearFilter, loadGifs } from "../redux/Giphy/giphy.action";
+import FilterWrapper from "./style";
+
+interface props {
+  filter: (args: any) => void;
+  clear: () => void;
+  load: () => void;
+  loading: boolean;
+  data: any;
+  error: boolean;
+}
+
+const Search: React.FC<props> = ({ filter, clear ,load}) => {
+  const [search, setSearch] = useState("");
+
+  // useEffect(() => {}, [clear]);
+
+  const onChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    let value = e.target.value
+    setSearch(value);
+    if (value === "") {
+      console.log('no more search')
+     clear()
+     // window.location.reload()
+    }
+  };
+
+  const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (search !== "") {
+      filter(search);
+      // setSearch("");
+    } else {
+      clear();
+      // load()
+    }
+  };
+
+  return (
+    <FilterWrapper className="form-inline col-8 " onSubmit={handleSearch}>
+      <input
+        type="text"
+        placeholder="Search all gifs and stickers"
+        aria-label="Search"
+        value={search}
+        onChange={onChangeInput}
+      />
+      <button type="submit">Search</button>
+    </FilterWrapper>
+  );
+};
+
+const mapStateToProps = (state: Istate) => ({
+  data: state.dataArray,
+  error: state.error,
+  loading: state.loading,
+});
+
+export default connect(mapStateToProps, {
+  filter: FilterGifs,
+  clear: ClearFilter,
+  load:loadGifs,
+  
+})(Search);
